efca8c81e028bfc5cfa71af0d8fe7aa02be62113
Zayd al-Muqaddim al-Qamar al-‘Aarabi <create@issue2.contact>
2024-09-19 07:17:54 +0300

    Framework.js added

---
+++ src/Framework.js
@@ -0,0 +1,18 @@
+const render = async (x, ...values) => {
+	var rendered = "";
+	for (let u = 0; u < x.length; u++) {
+		rendered = rendered.concat(x[u]);
+		if (u < x.length - 1) {
+			if (typeof values[u] == "function") {
+				rendered = rendered.concat(await values[u]());
+			} else {
+				rendered = rendered.concat(values[u]);
+			}
+		}
+	}
+
+	return rendered;
+};
+
+
+module.exports = {render}
